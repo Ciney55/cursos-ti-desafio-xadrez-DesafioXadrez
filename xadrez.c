581a67839cda7905c5d7c3315e1f7b67ce37d14a
Ciney55 <walcineysilva3@gmail.com>
2025-05-04 10:46:47 -0300

    criei a movimentação das peças torre, bispo e rainha.

--- xadrez.c
+++ xadrez.c
@@ -5,12 +5,35 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-// mover a torre 5 casas para a direita
+
+//mover a torre 5 casas para a direita
+
 int i = 0;
 
 for (i < 5; i++){
 
     printf("direita\n"); // imprime a direção do movimento 
-
-   return 0;
 }
+
+// mover o bispo 5 casas para cima e a direita 
+
+while (i <= 5){
+      printf("cima direita\n");
+      i++;
+}
+   
+// mover a rainha 8 casas a esquerda 
+
+do {
+   printf("esquerda\n");
+   i++;
+} while (i <= 8);
+   
+   
+   
+return 0;
+}
+
+
+
+
